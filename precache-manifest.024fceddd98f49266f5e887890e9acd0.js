2341c46dcd2af739e8d3c25f3f9903cd0a08c725
sreecharan reddy madireddy <136356575+sreecharanreddymadireddy@users.noreply.github.com>
2023-08-20 21:22:12 -0500

    Updates

---
+++ precache-manifest.024fceddd98f49266f5e887890e9acd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0aba6eb5a5239d941cff3fa72fe128",
+    "url": "/sreecharanreddymadireddy/bean_react.git/index.html"
+  },
+  {
+    "revision": "9707b028fb4085113e08",
+    "url": "/sreecharanreddymadireddy/bean_react.git/static/css/2.a58601c5.chunk.css"
+  },
+  {
+    "revision": "51b60731d4996fc090a5",
+    "url": "/sreecharanreddymadireddy/bean_react.git/static/css/main.10d47076.chunk.css"
+  },
+  {
+    "revision": "9707b028fb4085113e08",
+    "url": "/sreecharanreddymadireddy/bean_react.git/static/js/2.d05f64e8.chunk.js"
+  },
+  {
+    "revision": "f19f3396acd55fe19855badecd566256",
+    "url": "/sreecharanreddymadireddy/bean_react.git/static/js/2.d05f64e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b60731d4996fc090a5",
+    "url": "/sreecharanreddymadireddy/bean_react.git/static/js/main.6733a723.chunk.js"
+  },
+  {
+    "revision": "72c1ded88ae75e413825",
+    "url": "/sreecharanreddymadireddy/bean_react.git/static/js/runtime-main.3b8ec089.js"
+  }
+]);
